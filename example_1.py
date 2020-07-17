91f8c302bc980f63b0ac4f169f15f5821c00e322
GisangLee <masterkorea01@naver.com>
2020-07-17 19:38:13 +0900

    seaborn visualize

--- example_1.py
+++ example_1.py
@@ -1,12 +1,27 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv("data_sets/heart_disease_db.csv")
+data = pd.read_csv("data_sets/movies.csv")
 
 
 def get_data(data):
-    print("데이터 : ", data)
-    print("데이터 : ", data.shape)
-    print("데이터 컬럼 : ", list(data.columns))
+    print("데이터 정보 : \n", data.info())
+    print("데이터 NULL 합계 : ", data.isnull().sum())
+    print("데이터 top 5\n")
+    print(data.head())
+    print("데이터 서머리 : \n")
+    print(data.describe())
+    print("변수 : \n")
+    print(data.columns)
 
+
+data = data.drop(['homepage'], axis=1)
+
+print(data[['popularity', 'vote_average', 'production_companies']])
+
+ax = sns.barplot(x='vote_average', y='title', data=data.head(50))
+ax.plot()
+plt.show()
 
 get_data(data)
